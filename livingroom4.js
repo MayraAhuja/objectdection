9233ef5c11636bcbc7fa5bc5219ff48678d3ccd4
MayraAhuja <90937554+MayraAhuja@users.noreply.github.com>
2023-04-01 15:02:43 -0400

    Add files via upload

---
+++ livingroom4.js
@@ -0,0 +1,56 @@
+image1111="";
+modelStatus="";
+resultsArray=[];
+
+function preload() {
+    image1111=loadImage("livingroom.jpeg");
+}
+
+
+function setup() {
+    canvas=createCanvas(700,500);
+    canvas.center();
+    model=ml5.objectDetector("cocossd", ml);
+}
+
+
+function ml() {
+    console.log("model loaded!!!");
+    document.getElementById("status").innerHTML="Detecting objects...";
+    modelStatus=true;
+    model.detect(image1111, results);
+}
+
+
+function results(errorArray, resultsArray1) {
+    if (errorArray) {
+        console.error(errorArray);
+    } else {
+        console.log(resultsArray1);
+        resultsArray=resultsArray1;
+    }
+}
+
+
+function draw() {
+    image(image1111,0,0,700,500);
+    if (modelStatus != "") {
+      for (h=0; h<resultsArray.length; h=h+1){
+        document.getElementById("status").innerHTML="Objects Dectected!!!!";
+        stroke("fuchsia");
+        fill("fuchsia");
+        confidence= Math.floor(resultsArray[h].confidence*100); 
+        text(confidence+"% "+ resultsArray[h].label, resultsArray[h].x, resultsArray[h].y);
+        noFill();
+        stroke("fuchsia");
+        rect( resultsArray[h].x, resultsArray[h].y, resultsArray[h].width, resultsArray[h].height);
+      }
+      }  
+}
+
+function back() {
+    window.location="home.html";
+}
+
+
+    
---
+++ main.js
@@ -0,0 +1,16 @@
+function livingR() {
+    window.location="home 4.html";
+}
+
+
+function bathR() {
+    window.location="home 1.html";
+}
+
+function bedR() {
+    window.location="home 2.html";
+}
+
+function kitchenR() {
+    window.location="home 3.html";
+}
